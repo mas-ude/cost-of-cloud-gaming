8f876937705a44af87afb6d439593d1db8ffc04b
Florian Metzger <fm@tru42.org>
2016-03-02 16:28:50 +0100

    added two platform-separated game-lengths visualizations

--- scripts/R/howlongtobeat.R
+++ scripts/R/howlongtobeat.R
@@ -57,17 +57,39 @@ ggplot(df.steammetascorehltb, aes(x = score)) + stat_ecdf()
 #############################
 df.gfnow <- read.csv("gfnow-games.csv", header=TRUE, sep=",", colClasses=c("character", "numeric"))
 df.psnow <- read.csv("psnow-games.csv", header=TRUE, sep=";", colClasses=c("character", "numeric", "numeric", "numeric", "numeric", "logical"))
-
+df.steamdata <- read.csv(file="steamdata-20160206.csv", head=TRUE, sep=",", colClasses=c("numeric", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
 
 df.hltb$title <- tolower(df.hltb$title)
 df.gfnow$name <- tolower(df.gfnow$name)
 df.psnow$Title <- tolower(df.psnow$Title)
+df.steamdata$name <- tolower(df.steamdata$name)
 
 df.hltb.pc = subset(df.hltb, platform == "PC")
 df.hltb.ps = subset(df.hltb, platform %in% c("PlayStation", "PlayStation 2", "PlayStation 3"))
 
-
-
 df.consolidated.gfnow <- merge(df.gfnow, df.hltb.pc, by.x = "name", by.y = "title", all.x = TRUE)
-
 df.consolidated.psnow <- merge(df.psnow, df.hltb.ps, by.x = "Title", by.y = "title", all.x = TRUE)
+df.consolidated.steam <- merge(df.steamdata, df.hltb.pc, by.x = "name", by.y = "title", all.x = TRUE)
+
+## generate data frame for multi-plat density plot
+
+df.lengths <- data.frame()
+df.lengths <- rbind(df.lengths, data.frame(title = df.consolidated.psnow$Title, platform = "PlayStation Now", combined_length = df.consolidated.psnow$combined_length))
+df.lengths <- rbind(df.lengths, data.frame(title = df.consolidated.gfnow$name, platform = "Geforce Now", combined_length = df.consolidated.gfnow$combined_length))
+df.lengths <- rbind(df.lengths, data.frame(title = df.consolidated.steam$name, platform = "Steam", combined_length = df.consolidated.steam$combined_length))
+
+
+## density plot
+p <- ggplot(df.lengths, aes(x = combined_length, fill = as.factor(platform), color = as.factor(platform))) + geom_density(alpha = 0.4) + scale_x_log10() #+ xlim(0, 75)#+ scale_x_log10() # + xlim(0, 100)
+p <- p + xlab("avg. combined playthrough length (h)") + ylab("density")
+p <- p + theme(text = element_text(size=20))
+p
+ggsave("gamelengths-by-platform-density.pdf", width=12, height=8)
+
+## violins
+# p <- ggplot(df.lengths, aes(x = as.factor(platform), y = combined_length)) + geom_violin(scale = "count") + scale_y_log10() #+ xlim(0, 75)#+ scale_x_log10() # + xlim(0, 100)
+p <- ggplot(df.lengths, aes(x = as.factor(platform), y = combined_length)) + geom_violin(scale = "area") + geom_boxplot(width = 0.3) + scale_y_log10() #+ xlim(0, 75)#+ scale_x_log10() # + xlim(0, 100)
+p <- p + xlab("platform") + ylab("avg. combined playthrough length (h)")
+p <- p + theme(text = element_text(size=20))
+p
+ggsave("gamelengths-by-platform-violin.pdf", width=12, height=8)
